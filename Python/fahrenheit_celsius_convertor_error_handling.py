98dc23b4301e3f3c37b233af020c6a578dcb6f5b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:06:28 -0500

    Add files via upload

---
+++ Python/fahrenheit_celsius_convertor_error_handling.py
@@ -0,0 +1,15 @@
+# try except error handling:
+# program that converts temperature fahrenheit to celsius 
+
+temp = 32 #"5 degrees" # string
+cel = 0
+try:
+    fahr = float(temp) # cannot float a str
+    cel = (fahr-32.0) * 5.0/9.0
+except:
+    print('Error, enter a numerical number')
+    quit()
+print(cel)
+
+
+
