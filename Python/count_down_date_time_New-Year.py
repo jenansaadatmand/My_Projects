235b4701b343afe76ceae14d64eba6f08f076fb4
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:05:19 -0500

    Add files via upload

---
+++ Python/count_down_date_time_New-Year.py
@@ -0,0 +1,22 @@
+# Manipulating date and time object data, using a built in datetime library in python
+# A date countdown program that calculates the next date when provided with the (years, months, weeks, and times) as input 
+# pip install python-dateutil in terminal only once
+# you can create date and time objects, loop through a range of dates, parse and format date strings 
+# many uses of relativedelta to find out date count up, count down and time remaining or to apply any datetime calculation
+
+
+# Program calculates number of dates and time remaining for New Year:
+# check this website for other used of the python-dateutil package https://www.plus2net.com/python/date-relativedelta.php
+
+
+from dateutil.relativedelta import relativedelta
+from datetime import date
+# alternatively: you can import all package components by import *
+
+dt = date.today()
+print("Today:", dt)
+print("No of days left for New Year")
+
+dt2 = date(dt.year+1,1,1)
+dt3 = relativedelta(dt2, dt)
+print("Days:",dt3.days, " Months:", dt3.months, "Years:", dt3.years)
