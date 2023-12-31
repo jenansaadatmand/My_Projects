ce7fe5f93d3cb12776e1ec23fc6e1aa52713abad
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:07:24 -0500

    Add files via upload

---
+++ Python/seconds_to_minutes_converter.py
@@ -0,0 +1,6 @@
+# seconds to minutes converter 
+# Program to convert number of seconds into minutes for display
+# Use the floor division to round down the answer.
+seconds = int(input("Enter the number of seconds: "))
+display_minutes = seconds//60
+print(display_minutes, "minutes")
