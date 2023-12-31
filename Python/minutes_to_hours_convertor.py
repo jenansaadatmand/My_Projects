2fd6dde033e8f1ffb5d447870700be91f33db679
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:06:59 -0500

    Add files via upload

---
+++ Python/minutes_to_hours_convertor.py
@@ -0,0 +1,10 @@
+# Program converts minutes to hours
+# An expression that calculates how many hours there are in 160 minutes:
+minutes = input("Enter minutes:")
+minutes = int(minutes)
+hours = minutes / 60
+
+print(hours)
+print()
+
+
