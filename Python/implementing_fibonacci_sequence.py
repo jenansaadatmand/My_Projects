ca4e5605b5640b299e147c5663bdbb80c8044973
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2023-12-31 15:06:46 -0500

    Add files via upload

---
+++ Python/implementing_fibonacci_sequence.py
@@ -0,0 +1,4 @@
+# program for implementing a Fibonacci sequence
+# A Fibonacci sequence is defined as a set of numbers 
+# such that each number is the sum of the two preceding (previous) numbers, 
+# starting from 0 and 1.
