f6e19bc50bfc186cde62fbbb721cac252a2b807d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-03 14:21:20 -0500

    Update count_up_date_time_program.py

--- Python/count_up_date_time_program.py
+++ Python/count_up_date_time_program.py
@@ -1,7 +1,7 @@
 # Manipulating date and time object data, using a built-in datetime library and installing python-dateutil package in python
 # A date count-up program that calculates the next date when provided with the (years, months, weeks, and times) as input 
 # pip install python-dateutil in terminal only once
-# you can create date and time objects, loop through a range of dates, parse and format date strings 
+# you can create date and time objects, loop through a range of dates, parse, and format date strings 
 # many uses of relativedelta to find out date count up, count down and time remaining or to apply any datetime calculation
 
 from datetime import *
