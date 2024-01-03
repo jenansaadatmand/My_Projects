da6817ba9b4fe34b6d89775d545d8f24abd74e62
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-03 14:23:54 -0500

    Update minutes_to_seconds_converter.py

--- Python/minutes_to_seconds_converter.py
+++ Python/minutes_to_seconds_converter.py
@@ -1,6 +1,6 @@
 # Minutes to seconds converter
 # Program to convert minutes to seconds
-# Use the modulo % operstor to find the remainder.
+# Use the modulo % operator to find the remainder.
 minutes = int(input("Enter the number of minutes: "))
 seconds = minutes % 60
 print(seconds, "seconds")
