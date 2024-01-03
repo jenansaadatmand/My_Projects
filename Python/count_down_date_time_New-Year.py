945c3ada32fbba76169921c83f3798d5d8eb9113
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-03 14:15:21 -0500

    Update count_down_date_time_New-Year.py

--- Python/count_down_date_time_New-Year.py
+++ Python/count_down_date_time_New-Year.py
@@ -1,7 +1,7 @@
 # Manipulating date and time object data, using a built-in datetime library in Python
 # A date countdown program that calculates the next date when provided with the (years, months, weeks, and times) as input 
 # pip install python-dateutil in terminal only once
-# You can create date and time objects, loop through a range of dates, parse and format date strings 
+# You can create date and time objects, loop through a range of dates, parse, and format date strings 
 # Many uses of relativedelta to find out date count up, count down and time remaining or to apply any datetime calculation
 
 
