b233751988f03017481f1d8f279012948011694b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-05 03:44:56 -0500

    Update func_check_if_number_is_prime.py

--- Python/func_check_if_number_is_prime.py
+++ Python/func_check_if_number_is_prime.py
@@ -1,4 +1,12 @@
+# A prime number is a whole number greater than 1 whose only factors are 1 and itself. 
+# A factor is a whole number that can be divided evenly into another number. 
+# The first few prime numbers are 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.  
+# Numbers that have more than two factors are called composite numbers.
+
+
+
 # A function defined to check if a number is prime:
+
 def checkIfPrime (numberToCheck):
     for x in range(2, numberToCheck):
         if (numberToCheck%x == 0 ):
