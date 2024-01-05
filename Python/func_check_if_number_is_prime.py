0e8ca5e10e13fcaa87456f8c22cfabff19174eaa
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-05 03:40:27 -0500

    Update func_check_if_number_is_prime.py

--- Python/func_check_if_number_is_prime.py
+++ Python/func_check_if_number_is_prime.py
@@ -1,4 +1,4 @@
-# A function defined to check if a number is prime
+# A function defined to check if a number is prime:
 def checkIfPrime (numberToCheck):
     for x in range(2, numberToCheck):
         if (numberToCheck%x == 0 ):
