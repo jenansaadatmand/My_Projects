6378da789e0b6d98e69669b959da51be13f940b5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-01-06 13:20:45 -0500

    Update predict_user_age_in_a_year.py

--- Python/predict_user_age_in_a_year.py
+++ Python/predict_user_age_in_a_year.py
@@ -1,7 +1,7 @@
 # This program says Hello and asks for the user's name and age, then it predicts the user's age in a one-year timeline.
 
 print('Hello world')
-print('What is your name?')    # ask for their name
+print('What is your name?')    # Ask for their name
 myName = input()
 print('It is good to meet you, ' + myName)
 print('The length of your name is:')
