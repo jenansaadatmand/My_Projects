f8d43ae1d04dd9b469cfe860f66fd7fcf5df781b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:14:34 -0500

    Update calculate_a_facotrial_number.py

--- Python/calculate_a_facotrial_number.py
+++ Python/calculate_a_facotrial_number.py
@@ -1,4 +1,4 @@
-# A program or a function that calculates a facotrial of a number
+# A program or a function that calculates a factorial of a number
 # The factorial of a number is the product of all the integers from 1 to that number.
 
 # For example, the factorial of 6 is 1*2*3*4*5*6 = 720. Factorial is not defined for negative numbers, and the factorial of zero is one, 0! = 1.
