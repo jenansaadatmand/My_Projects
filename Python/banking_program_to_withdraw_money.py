ddd94820a499da28fa48240456b5bc3ae00a04e1
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:07:07 -0500

    Add files via upload

---
+++ Python/banking_program_to_withdraw_money.py
@@ -0,0 +1,16 @@
+# A banking program to withdraw money from an ATM, and displays current balance. 
+# Make the program report a message if balance falls below threshold limit of $50
+print()
+
+def withdraw_money(current_balance, amount): # function takes two parameters
+    if (current_balance >= amount):
+        current_balance = current_balance - amount
+        return current_balance  # functions returns a value
+
+
+balance = withdraw_money(100,80)  # provide two positional arguments, which are preset earlier in the function's definition 
+
+if balance <= 50:
+    print("We need to make a deposit")
+else:
+    print("Nothing to see here!")
