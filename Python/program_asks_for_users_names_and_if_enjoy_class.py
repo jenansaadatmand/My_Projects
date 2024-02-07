c3d97a45d4e3a7cb8276bd054cb87fc159d0f7aa
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:12:12 -0500

    Add files via upload

---
+++ Python/program_asks_for_users_names_and_if_enjoy_class.py
@@ -0,0 +1,12 @@
+# A program that greets the users, asks for users names and if they enjoy the course
+ 
+print("Hi!")
+name = input("What's your name? ")
+
+print("It's nice to meet you,", name)
+answer = input("Are you enjoying the course? ")
+
+if answer == "Yes":
+    print("That's good to hear!")
+else:
+    print("Oh no! That makes me sad!")
