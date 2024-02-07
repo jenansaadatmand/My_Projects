10332bea9b923fb5b1d2a6bcf8361b3a1a4e0621
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:09:46 -0500

    Add files via upload

---
+++ Python/printing_absolute_value_of_a_number.py
@@ -0,0 +1,6 @@
+# Printing absolute value of a number
+num = int(input("Enter a number:"))
+if (num >= 0):
+    print(num)  # prints a positive number
+else:
+    print(-1 * num) # prints the absolute value of a negative number
