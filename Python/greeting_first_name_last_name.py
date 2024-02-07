ada45708eef24c68a87c45b59865a17df11d993e
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-07 14:00:58 -0500

    Update greeting_first_name_last_name.py

--- Python/greeting_first_name_last_name.py
+++ Python/greeting_first_name_last_name.py
@@ -1,4 +1,4 @@
-# Program that prints first name and last name and greets the user
+# Program that prints the first name and last name and greets the user
 first_name = 'Christopher'
 last_name = 'Harrison'
 print(first_name + last_name)
