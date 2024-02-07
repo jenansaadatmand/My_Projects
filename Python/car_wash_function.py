b88ec3c8df7a80d20d35609d76ee9b02066a3a73
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:10:48 -0500

    Add files via upload

---
+++ Python/car_wash_function.py
@@ -0,0 +1,8 @@
+def wash_car():
+    package = input("Enter package:")
+    if package == "basic":
+        print("Your Basic wash includes: \n White foam,\n One rinse,\n and a Drip dry")
+    else: 
+        print("Your Platinum Wash includes: \n Tricolor foam, \n Two rinses, \n and a Blow dry ")
+
+wash_car()
