b6b3562310e26205c72d5b0f76bafb0456bf8d18
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-06 23:16:13 -0500

    Update calculate_a_facotrial_number.py

--- Python/calculate_a_facotrial_number.py
+++ Python/calculate_a_facotrial_number.py
@@ -7,7 +7,7 @@
 # A factorial: is a function that multiplies a number by every number below it till 1. 
 # For example, the factorial of 3 represents the multiplication of numbers 3, 2, 1, i.e. 3! = 3 × 2 × 1 and is equal to 6.
 
-def factorial(n):  # A function 
+def factorial(n):  # Defining a function 
     if n < 0:
         return "Error: n must be a non-negative integer"
     elif n==0 or n == 1:
