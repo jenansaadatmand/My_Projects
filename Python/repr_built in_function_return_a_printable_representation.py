511488f5794b6474ca1ccd5a374089d6ebe8a256
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 18:54:22 -0500

    Update repr_built in_function_return_a_printable_representation.py

--- Python/repr_built in_function_return_a_printable_representation.py
+++ Python/repr_built in_function_return_a_printable_representation.py
@@ -1,4 +1,4 @@
-# repr() is a built in function, it return a string containig a printable representation of an object. (return printable representation). For many type values, it returns a string object, exactly like str()
+# repr() is a built-in function, it return a string containig a printable representation of an object. (return printable representation). For many type values, it returns a string object, exactly like str()
 
 s = "3x"
 str(s)
