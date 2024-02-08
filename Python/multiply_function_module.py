ee333d23c643ec19a47c6935c038c3a974a49a45
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 18:51:56 -0500

    Update multiply_function_module.py

--- Python/multiply_function_module.py
+++ Python/multiply_function_module.py
@@ -1,6 +1,6 @@
-def mult(x, y):  #  a function that multiplies two numbers 
-    print(f'{x} * {y} = {x * y}') # string formating with f-string and {} pace holders (formated string literal)
+def mult(x, y):  #  a function that multiplies two numbers as parameters
+    print(f'{x} * {y} = {x * y}') # string formatting with f-string and {} pace holders (formatted string literal)
     
-# mult(2, 4) # to call the function 
+# mult(2, 4) # to call the function with two positional parameters
 
 
