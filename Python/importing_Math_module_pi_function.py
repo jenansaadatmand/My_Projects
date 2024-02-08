98fc463da7f87c2cf5b7f45a676dc62c37e40892
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 18:56:20 -0500

    Update importing_Math_module_pi_function.py

--- Python/importing_Math_module_pi_function.py
+++ Python/importing_Math_module_pi_function.py
@@ -1,4 +1,3 @@
 # importing Python Math module, to have access to pi function
 import math
 print(f"The value of pi is approximately {math.pi:.3f}")  # .3f to round pi and specify a 3 places after the decimal point
-
