4fcd892df83de09e41e33b0ac4023f3c102b1b7d
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 01:25:18 -0500

    Add files via upload

---
+++ Python/perform_mathematical_operations.py
@@ -0,0 +1,18 @@
+# Perform mathematical operations
+result = 2**3
+print("2**3=", result)
+
+print("Practice Exercise 1:")
+
+
+# Perform mathematical operations
+# 3**2 is 3^2 (or 3 squared) and 2**3 means 2^3 (or 2 cubed).
+
+answer = 5+2**3
+print("The ansswer is:", answer)
+
+result = 5-3 
+print("5 - 3 =", result)
+
+print("Done!")
+
