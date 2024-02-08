5e7c89390533eb6ce6447b974a91f83cd7944714
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 01:24:41 -0500

    Add files via upload

---
+++ Python/user_registration_input.py
@@ -0,0 +1,7 @@
+name = input("Hi, What's your name?")
+age = int(input("how old are you?"))
+
+if age < 13:
+    print("You're too young to register", name)
+else: 
+    print("Feel free to join", name)
