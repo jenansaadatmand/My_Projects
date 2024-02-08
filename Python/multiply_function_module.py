6a0b3dc53fdd967aa147bd5906403649c5d7a275
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-08 18:50:59 -0500

    Add files via upload

---
+++ Python/multiply_function_module.py
@@ -0,0 +1,8 @@
+def mult(x, y):  #  a function that multiplies two numbers 
+    print(f'{x} * {y} = {x * y}') # string formating with f-string and {} pace holders (formated string literal)
+    
+
+
+mult(2, 4) # to call the function 
+
+
