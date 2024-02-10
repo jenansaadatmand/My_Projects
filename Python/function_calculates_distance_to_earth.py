f49cac2840866be35483cd3561681e6e4a32fc05
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-10 00:24:58 -0500

    Add files via upload

---
+++ Python/function_calculates_distance_to_earth.py
@@ -0,0 +1,16 @@
+# A function that calculates the distance of a planet to earth
+# A function that requires one argument to be passed to it
+
+def distance_from_earth(destination):
+    if destination == "Moon":
+        return "238,855"
+    else: # catch-all conditions
+        return "unable to compute that destination" 
+print(distance_from_earth("Moon"))
+print()
+print(distance_from_earth("Saturn"))
+print()
+
+# Functions as arguments 
+
+
