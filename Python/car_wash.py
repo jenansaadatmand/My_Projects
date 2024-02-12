79e106fcdacee805bdcb552a393f584a4ef297f9
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-12 16:18:05 -0500

    Add files via upload

---
+++ Python/car_wash.py
@@ -0,0 +1,36 @@
+
+def wash_car(amount_paid): # added a parameter to allow the function to change its behavior based on some input
+
+    if (amount_paid == 12):  # platinum wash
+        print("Wash with tri-color foam")
+        print("Rinse twice")
+        print("Dry with large blow dryer")
+
+    if (amount_paid == 6):  # basic wash
+        print("Wash with white foam")
+        print("Rinse once")
+        print("Air dry") 
+
+wash_car(12) # define the postiional argument (values to the defined parameter), keep changing the positional argument to change the output behavior of the function  
+
+
+
+print()
+
+# Solution 2:
+amount_paid = 12
+
+
+def wash_car():
+    if (amount_paid == 12):  # platinum wash
+        print("Wash with tri-color foam")
+        print("Rinse twice")
+        print("Dry with large blow dryer")
+
+    if (amount_paid == 6):  # basic wash
+        print("Wash with white foam")
+        print("Rinse once")
+        print("Air dry") 
+
+wash_car()
+
