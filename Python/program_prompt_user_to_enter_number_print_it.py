e7969c7cb6ad1271efae025cb42813a74e5a33ea
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-18 11:10:39 -0500

    Add files via upload

---
+++ Python/program_prompt_user_to_enter_number_print_it.py
@@ -0,0 +1,6 @@
+# Program guesses the number 
+
+value = input('Enter a number: ') # input() always stores the value as a string
+print(value)
+
+print("the number you have entered is:", value) # concatenation of two strings
