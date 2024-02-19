fa8585ce91dfbb4f9948ee7d405616db855130bd
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-19 11:18:55 -0500

    Update for_loop_to_count_from_0_to_10.py

--- Python/for_loop_to_count_from_0_to_10.py
+++ Python/for_loop_to_count_from_0_to_10.py
@@ -1,4 +1,4 @@
-# A for loop that prints values from 0 to 9, not including 10, if you don't specify the beginig the program starts at 0
+# A for loop that prints values from 0 to 9, not including 10, if you don't specify the beginning the program starts at 0
 
 for value in range(10): # prints 1 to 9 not including 10
     print(value)
