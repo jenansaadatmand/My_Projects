d3b86fe65783edb44591f5663bc80778064436ef
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-19 11:16:07 -0500

    Add files via upload

---
+++ Python/convert_miles_to_kilometers.py
@@ -0,0 +1,15 @@
+# A program that converts miles to kilometers
+
+miles = input('Enter a distance in miles: ')
+# kilometers_value = miles_value * 1.609344
+
+# 1. Take the value entered by a User
+# 2. Convert it to a value in kilometers
+# 3. Print the result to the terminal 
+# - Text description
+# String concatenation 
+# Use float() to convert the string to a float number with decimal points
+
+miles_value = float(miles) # Convert string from input() to float() for calculations
+kilometers_value = miles_value * 1.609344  # Convert miles to kilometers
+print("The number you have entered is converted from " + "Miles to kilometers:", kilometers_value)
