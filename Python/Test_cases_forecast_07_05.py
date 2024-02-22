6c10d4d5d0b12b6beb8ab67a14d49900077d91bf
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-21 19:27:25 -0500

    Add files via upload

---
+++ Python/Test_cases_forecast_07_05.py
@@ -0,0 +1,18 @@
+# Test cases: Commands or scripts designed to test a specific scenario
+
+
+def check_temp(temp):
+    if  temp < 15:
+        print('Bring a jacket')
+    elif temp > 25 and temp <= 35:
+        print('Pack a jacket')
+    elif temp > 35:
+        print('Leave the jacket at home')
+
+# Test case by recalling the function itself with different values that fall within the range of each conditional statement to test them
+        # I don't want to leave a jacket at home for a temperature below 15
+print()
+
+check_temp(10) # excutes the first condition and stop
+check_temp(30) # execute the second condition and stop
+check_temp(37) # execute the third condition 
