8c1cc34d995354185793a5fae06fc557ee6e88e5
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-23 11:27:33 -0500

    Update function_with_variable_number_of_arguments.py

--- Python/function_with_variable_number_of_arguments.py
+++ Python/function_with_variable_number_of_arguments.py
@@ -1,7 +1,7 @@
 # Function with a variable number of arguments
 # * character denotes variable arguments (args)
 
-def multi_add(*args): # function with a variable number of arguments, *args means variable argumenrs
+def multi_add(*args): # function with a variable number of arguments, *args means variable arguments
     result = 0 # Define a variable that defaults to 0
     for x in args: # the function loops over each argument, and stores the result in x index or item 
         result = result + x # add them all to a running total
