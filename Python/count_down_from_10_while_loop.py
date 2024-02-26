e09948244fee447dd299aad77f46dd0b11e76b7b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-26 17:10:51 -0500

    Add files via upload

---
+++ Python/count_down_from_10_while_loop.py
@@ -0,0 +1,9 @@
+# a while loop that counts down from 10
+# Always start with setting the starting counter
+# Program will start from 10 but does not include 10 when printing the count down to screen
+
+i = 10
+
+while i > 0:
+    i -= 1 # decrement by 1
+    print(i)
