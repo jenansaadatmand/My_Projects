b095e526d52661cecfce3d1615058f5bfbd51fbe
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-26 17:11:48 -0500

    Update count_down_from_10_while_loop.py

--- Python/count_down_from_10_while_loop.py
+++ Python/count_down_from_10_while_loop.py
@@ -1,4 +1,4 @@
-# a while loop that counts down from 10
+# A while loop that counts down from 10
 # Always start with setting the starting counter
 # The program will start from 10 but does not include 10 when printing the count-down to the screen
 
