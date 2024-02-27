f83a93c6b14662ace5953a62a28e615ef9db66e3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-27 11:27:40 -0500

    Update object_oriented_programming_list.py

--- Python/object_oriented_programming_list.py
+++ Python/object_oriented_programming_list.py
@@ -2,7 +2,7 @@
 # OOP is an approach to organize or structure your code. It breaks a program into smaller parts known as objects using the Class blueprint
 # Objects can communicate with each other to make a program function but the divisioninto smaller units makes the code easier to reuse and to maintain
 # Each of these objects has its unique attributes/properties and behaviours/methods. 
-# Attributes are the data that the object has and Behaviours are someting that the object can do
+# Attributes are the data that the object has and Behaviours are something that the object can do
 # You create objects using a blueprint known as class
 # A class describes the types of attributes and behaviours that an object should have. You can use the same class to build multiple objects based on the same pattern but containing different property values.
 # eg. a class that created list objects 
