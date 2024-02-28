ce0f6814ff0dcd576664b568750d49da0a51c9e9
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-28 01:59:50 -0500

    Update Regex_search_for_pattern_in_string.py

--- Python/Regex_search_for_pattern_in_string.py
+++ Python/Regex_search_for_pattern_in_string.py
@@ -5,7 +5,7 @@ nine_digit_zip = '98101-0003'
 phone_number = '234-567-8901'
 
 # write a regular expression, which will identify five digits in a row, which is the minimum for a US zip code
-# Use the letter r to indicate that the string that follows may include back slashes that the compiler should ignore.
+# Use the letter r to indicate that the string that follows may include backslashes that the compiler should ignore.
 # Include regular expression in single quotes, then type back slash, d for digit, '\d{5}', denoting five occurrences for the preceding expression, the digit.
 
 five_digit_expression = r'\d{5}' # Created a regular expression
