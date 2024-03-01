46b0b2023bbea62f2ba05859fe5e488e93e8e0da
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-01 12:29:16 -0500

    Update program_finds_slices_string.py

--- Python/program_finds_slices_string.py
+++ Python/program_finds_slices_string.py
@@ -6,11 +6,10 @@ first_name_cap = first_name.capitalize()  # Using method capitalize()
 last_name_cap = last_name.capitalize()
 print(first_name_cap)
 print(last_name_cap)
-award_location = note.find('award: ')   # finding a string using .find() method
+award_location = note.find('award: ')   # Finding a string using .find() method
 print(award_location)  # you will get a value of index 0 indicating that the award is at the start of the string
 print()
 
 print(note[0:7])  # award starts at index 0 to 7 not including 7
 award_text = note[7:]  # Slice the remainder, we know that the award starts at index 0 to 6, so we slice from 7 to the end
 print(award_text)
-
