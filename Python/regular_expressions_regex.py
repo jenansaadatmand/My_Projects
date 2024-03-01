f9d556b6abb33cef8bb3db214704790ffae1ddf6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-02-29 22:40:33 -0500

    Update regular_expressions_regex.py

--- Python/regular_expressions_regex.py
+++ Python/regular_expressions_regex.py
@@ -4,7 +4,7 @@ five_digit_zip = '98101'
 nine_digit_zip = '98101-0003'
 phone_number = '234-567-8901'
 
-# write a regular expression, which will identify five digits in a row, which is the minimum for a US zip code
+# Write a regular expression, which will identify five digits in a row, which is the minimum for a US zip code
 # Use the letter r to indicate to the string that follows may include backslashes that the compiler should ignore.
 # Include regular expression in single quotes, then type back slash, d for digit, '\d{5}', denoting five occurrences for the preceding expression, the digit.
 
