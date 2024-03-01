9c83ae741bd495eb28edef47347157040a1175d7
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-01 12:28:23 -0500

    Update program_finds_slices_string.py

--- Python/program_finds_slices_string.py
+++ Python/program_finds_slices_string.py
@@ -2,7 +2,7 @@ first_name = 'malala'
 last_name = 'yousafzai'
 note = 'award: Nobel Peace Prize'
 
-first_name_cap = first_name.capitalize()  # using method capitalize()
+first_name_cap = first_name.capitalize()  # Using method capitalize()
 last_name_cap = last_name.capitalize()
 print(first_name_cap)
 print(last_name_cap)
