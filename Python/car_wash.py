662d474fa35ce46cfe1ff26b965840a5b4643ed2
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-01 21:45:34 -0500

    Update car_wash.py

--- Python/car_wash.py
+++ Python/car_wash.py
@@ -1,5 +1,5 @@
 
-def wash_car(amount_paid): # added a parameter to allow the function to change its behavior based on some input
+def wash_car(amount_paid): # added a parameter to allow the function to change its behaviour based on some input
 
     if (amount_paid == 12):  # platinum wash
         print("Wash with tri-color foam")
