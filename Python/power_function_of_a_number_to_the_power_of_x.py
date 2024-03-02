ef276d8cc5b3123851e200fae5a43dde9956906c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-02 11:28:43 -0500

    Update power_function_of_a_number_to_the_power_of_x.py

--- Python/power_function_of_a_number_to_the_power_of_x.py
+++ Python/power_function_of_a_number_to_the_power_of_x.py
@@ -1,6 +1,6 @@
 # A Power function that takes the number and returns the number after raisinf it with the given power 
 
-# This is a function with default value for an argument
+# This is a function with the default value for an argument
 # This function that takes a number and raises it to the given power
 
 def power(num, x = 1): # function with a default argument 
