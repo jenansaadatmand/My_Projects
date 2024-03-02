b5dc44c213d280f2b39fc4200ad42a8de4b21cb3
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-01 21:46:29 -0500

    Update car_wash.py

--- Python/car_wash.py
+++ Python/car_wash.py
@@ -11,7 +11,7 @@ def wash_car(amount_paid): # Added a parameter to allow the function to change i
         print("Rinse once")
         print("Air dry") 
 
-wash_car(12) # Define the positional argument (values to the defined parameter), and keep changing the positional argument to change the output behavior of the function  
+wash_car(12) # Define the positional argument (values to the defined parameter), and keep changing the positional argument to change the output behaviour of the function  
 
 
 
