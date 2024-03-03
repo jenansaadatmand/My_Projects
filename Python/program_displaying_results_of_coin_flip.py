4e7d01a7d64fd3fbb8a65235b604083cd876572c
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-03 01:33:25 -0500

    Update program_displaying_results_of_coin_flip.py

--- Python/program_displaying_results_of_coin_flip.py
+++ Python/program_displaying_results_of_coin_flip.py
@@ -9,6 +9,6 @@ flips = ['heads',
 
 print(flips) # prints all the list
 
-print(flips.count('heads')) # use count() method to return the number of elements that matches a certain string 'heads'
+print(flips.count('heads')) # use count() method to return the number of elements that match a certain string 'heads'
 
 print(flips.pop()) # prints the last item in the list using pop() method
