67a8b2e451350e63d916356c99d8d7f8a58d36b8
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-04 13:17:30 -0500

    Update program_read_input_save_output_into_another_file.py

--- Python/program_read_input_save_output_into_another_file.py
+++ Python/program_read_input_save_output_into_another_file.py
@@ -2,7 +2,7 @@
 # Performs some processing and then 
 # Writes output to another file
 # It uses a few Python methods that are specific to input and output
-# The code in the first couple lines uses the open method,this opens files with specific names that are located in the same folder as the program file
+# The code in the first couple lines uses the open method, which opens files with specific names that are located in the same folder as the program file
 # The values.txt file in the files list. When opening this file, it contains a list of numbers, notice that the file list doesn't contain a values-total.txt file. 
 
 
