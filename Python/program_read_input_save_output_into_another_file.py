ecd5f3d12366d9c4dfabb185384da37d1b50f4c6
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-04 13:18:15 -0500

    Update program_read_input_save_output_into_another_file.py

--- Python/program_read_input_save_output_into_another_file.py
+++ Python/program_read_input_save_output_into_another_file.py
@@ -15,5 +15,5 @@ for line in infile:    # Taking one line at a time, a number at a time
     sum += int(line) # Converting it to integer, adding them up with + operator and assigning it to Sum variable  
     print(line.rstrip(), file=outfile) # Printing current line with right space strip character, and specifying to save data to a file, output to outfile variable, which is a file that will be created
 print('\nTotal: ' + str(sum), file=outfile) # convert to string to concatenate strings, store in outfile
-outfile.close()  # close the outfile
+outfile.close()  # Close the outfile
 print('Output complete')
