c0007e0094f648fed10e0a9f191622367b0ec63b
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-06 10:39:27 -0500

    Update function_multiplies_two_numbers.py

--- Python/function_multiplies_two_numbers.py
+++ Python/function_multiplies_two_numbers.py
@@ -1,2 +1,4 @@
+# Defining a function that multiplies two numbers together to be used in function_multiplies_two_numbers.py exercise
+
 import testmodule as tm # assigning the module to a tm shortcut
 tm.mult(10, 5) # referencing the module.function()
