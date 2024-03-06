e2fffca866ef0c9a5c2e5e83c51528af5433a726
Jenan Saadatmand <153618882+jenansaadatmand@users.noreply.github.com>
2024-03-06 10:35:37 -0500

    Update testmodule.py

--- Python/testmodule.py
+++ Python/testmodule.py
@@ -1,4 +1,4 @@
-def mult(x, y):  #  a function that multiplies two numbers 
+def mult(x, y):  #  A function that multiplies two numbers 
     print(f'{x} * {y} = {x * y}') # string formating with f-string and {} pace holders (formated string literal)
     
 
